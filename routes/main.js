1e7777d2adc3260bbadd37a86efc580f6df11f1c
furkansimsekli <simseklifurkan0@gmail.com>
2022-09-23 19:59:02 +0300

    added routes

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,12 @@
-function index(req, res) {
-    res.send("");
-}
+const express = require("express");
+const controller = require("../controllers/main");
+const router = express.Router();
 
-module.exports = index;
+router.get('/', controller.renderHomePage);
+router.get('/about', controller.renderAboutPage);
+router.get('/faq', controller.renderFaqPage);
+router.get('/events', controller.renderEventsPage);
+router.get('/arge', controller.renderArgePage);
+router.get('/forms/:form', controller.renderFormPage);
+
+module.exports = router;
